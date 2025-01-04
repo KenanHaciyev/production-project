7e7a74f6e80db41c7901ca501a29d90edfd240d2
Kenan <kenan.haciyev.123@bk.ru>
2025-01-04 20:37:58 +0400

    letter to titleCase

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,6 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import cls from './notFoundPage.module.scss';
+import cls from './NotFoundPage.module.scss';
 
 interface notFoundPageProps {
     className?: string;
